80ac8486f85c824bc1761e23df8cf37d98e4420b
UzDevid <uzdevid@gmail.com>
2023-04-07 18:23:14 +0500

    Добавил проверку

--- merchant/Merchant.php
+++ merchant/Merchant.php
@@ -35,7 +35,7 @@ class Merchant extends Controller {
             return $error;
         }
 
-        if ($error = $this->checkAccount($this->payload)) {
+        if ($error = $this->checkAccount($this->payload) !== true) {
             return $error;
         }
 
